03e36ee3fc520075ea9b20442f7866ef1ed71708
yuban12315 <fengyulin@imudges.com>
2016-03-14 13:36:43 +0800

    完善

--- js/regist.js
+++ js/regist.js
@@ -1,25 +1,41 @@
 $(document).ready(function () {
-    var url = '../PHP/Service/UserService/Register.php';
+    var url = '../PHP/Service/UserService/Vcode.php';
+    $("#Vcode").click(function () {
+        $.post('../PHP/Service/UserService/Vcode.php', {
+            userphone: $("#mobile").val()
+        }, function (data) {
+            if (data[0] != '' || data[0] != null) {
+                var msg = '<p>' + data + '</p>';
+                $("#wrong").innerHTML = msg;
+                document.getElementById('wrong').innerHTML = msg;
+                $("#alert").click();
+            }
+        });
+    });
+
+    url = '../PHP/Service/UserService/Register.php';
     $("#regist").click(function () {
-        var username=$("#username").val();
-        var password=$("#password").val();
-        var password2=$("#password2").val();
-        var mobile=$("#mobile").val();
-        var sex=$("#sex").val();
-        $.post(url,{
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var password2 = $("#password2").val();
+        var mobile = $("#mobile").val();
+        var vcode = $("#code").val();
+        var sex = $("#sex").val();
+        $.post(url, {
             username: username,
             password: password,
-            repassword:password2,
-            usersex:sex,
-            userphone:mobile
+            repassword: password2,
+            usersex: sex,
+            userphone: mobile,
+            vcode: vcode
         }, function (data, status) {
-            if(data[0]=='1'){
+            if (data[0] == '1') {
                 url = 'http://127.0.0.1/LoveUniversity/php/Service/UserService/Session.php';
                 $.getJSON(url, function (data) {
-                    var id =data.userid;
-                    setCookie('username',username);
-                    setCookie('userid',id);
-                    location.href='../index.html';
+                    var id = data.userid;
+                    setCookie('username', username);
+                    setCookie('userid', id);
+                    location.href = '../index.html';
                 });
             }
             else {
