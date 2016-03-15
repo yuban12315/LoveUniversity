3e4315c15704ed38acf632d81bc2ef7d3fd5ba77
yourname <liujiannan@imudges.com>
2016-03-15 16:39:12 +0800

    修改头像

--- PHP/Service/UserService/ModifyPhoto.php
+++ PHP/Service/UserService/ModifyPhoto.php
@@ -23,7 +23,7 @@ function MP($str)
         $src = $str;
         $mm = randnum(5);
         $mm = get_md5_string($mm);
-        $path = 'http://7xrqhs.com1.z0.glb.clouddn.com/' . $mm;
+        $path = 'http://7xrqhs.com1.z0.glb.clouddn.com/' . $mm .'.png';
         upload($src, $path, 'loveu');
         //unlink($src);
         $str = "update user set UserPhoto = '{$path}' where UserId = '{$userid}'";
