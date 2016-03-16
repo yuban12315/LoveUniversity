578fb2cb5aec9b8f0c90e17f2b945990aa5cf358
yuban12315 <fengyulin@imudges.com>
2016-03-16 12:42:57 +0800

    完善

---
+++ js/index-service.js
@@ -0,0 +1,16 @@
+function Loaded() {
+    if (getCookie('userid')) {
+        resetCookie();
+    }
+    var user = getCookie('username');
+    if (user == null || user == '') {
+        var msg = " <ul class=\"nav pull-right navbar-nav\" id=\"user\">" +
+            " <li><a> <span class=\"glyphicon glyphicon-search\"></span></a></li>" +
+            " <li><a onclick=\"location.href='../login.html'\">登录</a></li>" +
+            "<li><a onclick=\"location.href='../regist.html'\">注册</a></li> </ul>"
+        document.getElementById('user').innerHTML = msg;
+    }
+    else {
+        document.getElementById('username').innerHTML = user + "<span class=\"caret\"></span>"
+    }
+}
