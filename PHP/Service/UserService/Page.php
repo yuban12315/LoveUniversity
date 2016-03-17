d43094afc78edbe51eb8ddcb4ada249ead7d796e
yourname <liujiannan@imudges.com>
2016-03-17 21:43:49 +0800

    wanshan

--- PHP/Service/UserService/Page.php
+++ PHP/Service/UserService/Page.php
@@ -15,20 +15,21 @@ function Mypage($table, $page, $time, $ID)
     $result = mysqli_query($conn, $str);
     $row = mysqli_fetch_assoc($result);
     $i = 0;
+
     echo '[';
     while ($i <= ($page * 10 - 1) && $row) {
 
-                if(strtotime(date("y-m-d h:i:s"))>=strtotime($row[$time]))
-                {
-                    $id = $row[$ID];
-                    $str = "update {$table} set state = 0 where {$ID} = '{$id}'";
-                    up($str);
-                }
-                if($row['state']==0||strtotime(date("y-m-d h:i:s"))>=strtotime($row[$time]))
-                {
-                    $row = mysqli_fetch_assoc($result);
-                    continue;
-                }
+/*
+        if (strtotime(date("y-m-d h:i:s")) >= strtotime($row[$time])) {
+            $id = $row[$ID];
+            $str = "update {$table} set state = 0 where {$ID} = '{$id}'";
+            up($str);
+        }
+        if ($row['state'] == 0 || strtotime(date("y-m-d h:i:s")) >= strtotime($row[$time])) {
+            $row = mysqli_fetch_assoc($result);
+            continue;
+        }*/
+        unset($row['GetUser']);
         $i++;
         if ($i >= ($page - 1) * 10) {
             echo ch_json_encode($row);
@@ -36,6 +37,6 @@ function Mypage($table, $page, $time, $ID)
         }
         $row = mysqli_fetch_assoc($result);
     }
-    echo '{"Num":"'.($i%10+1).'"}';
+    echo '{"Num":"' . ($i % 10 + 1) . '"}';
     echo "]";
 }
