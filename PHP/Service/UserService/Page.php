44622b41318b5ca5691ca8d5bb0f2f623b073159
yourname <liujiannan@imudges.com>
2016-03-18 13:49:39 +0800

    wanshan

--- PHP/Service/UserService/Page.php
+++ PHP/Service/UserService/Page.php
@@ -14,7 +14,7 @@ function Mypage($table, $page, $time, $ID)
     $result = mysqli_query($conn, $str);
     $row = mysqli_fetch_assoc($result);
     $i = 0;
-    while ($i <= ($page * 10 - 1) && $row) {
+    while ($i <= ($page * 10 - 1) && !empty($row)) {
         /*
         if (strtotime(date("y-m-d h:i:s")) >= strtotime($row[$time])) {
             $id = $row[$ID];
@@ -33,7 +33,15 @@ function Mypage($table, $page, $time, $ID)
         }
         $row = mysqli_fetch_assoc($result);
     }
-    $array['Num'] = ($i%10)+1;
+    if($i%10==0&&$i!=0)
+    {
+        $sum = 10;
+    }
+    else
+    {
+        $sum=$i%10;
+    }
+    $array['Num'] = ($sum);
     echo ch_json_encode($array);
 
 }
