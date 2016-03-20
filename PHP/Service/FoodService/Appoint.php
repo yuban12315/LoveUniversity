f169d134a2f5dfcd6580d36970ba6e18b3dae123
yourname <liujiannan@imudges.com>
2016-03-20 16:37:12 +0800

    wanshan

--- PHP/Service/FoodService/Appoint.php
+++ PHP/Service/FoodService/Appoint.php
@@ -4,7 +4,7 @@ session_start();
 require_once '../../DAO/DAO.php';
 if (isset($_SESSION['userid'])) {
     $getuser = $_SESSION['userid'];
-    $str = "select * from run where GetUser = {$getuser} and state = 1";
+    $str = "select * from run where GetUser = {$getuser}";
     $row = sel($str);
     if ($row && (strtotime(date("y-m-d h:i:s")) < strtotime($row['FoodTime']))) {
         echo '已有约会';
--- PHP/Service/RunService/appoint.php
+++ PHP/Service/RunService/appoint.php
@@ -10,7 +10,7 @@ session_start();
 require_once '../../DAO/DAO.php';
 if (isset($_SESSION['userid'])) {
     $getuser = $_SESSION['userid'];
-    $str = "select * from run where GetUser = {$getuser} and state = 1";
+    $str = "select * from run where GetUser = {$getuser}";
     $row = sel($str);
     if ($row && (strtotime(date("y-m-d h:i:s")) < strtotime($row['RunTime']))) {
         echo '已有约会';
