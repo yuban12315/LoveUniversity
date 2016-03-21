f38e240a5944290913ebbf0e0acaa81a6d259994
yourname <liujiannan@imudges.com>
2016-03-21 12:37:24 +0800

    wanshan

--- PHP/Service/UserService/Total.php
+++ PHP/Service/UserService/Total.php
@@ -8,19 +8,7 @@
 require_once '../../DAO/DAO.php';
 function Total($table)
 {
-    $conn = connect();
-    $str = "select * from {$table} where state = 1";
-    $result = mysqli_query($conn, $str);
-    $row = mysqli_fetch_assoc($result);
-    $total = 0;
-    while ($row) {
-        $total++;
-        $row = mysqli_fetch_assoc($result);
-    }
-    if ($total % 10 == 0) {
-        $total = floor($total / 10);
-    } else {
-        $total = floor($total / 10) + 1;
-    }
-    echo $total;
+    $str = "select count(*) from {$table} where state = 1";
+    $row = sel($str);
+    echo $row['count(*)'];
 }
