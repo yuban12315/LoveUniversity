0dc3b032d0cd6c47249b5af13d57a3eac4b25c21
yourname <liujiannan@imudges.com>
2016-03-21 16:13:03 +0800

    wanshan

--- PHP/Service/UserService/Total.php
+++ PHP/Service/UserService/Total.php
@@ -10,5 +10,11 @@ function Total($table)
 {
     $str = "select count(*) from {$table} where state = 1";
     $row = sel($str);
-    echo $row['count(*)'];
+    $total = $row['count(*)'];
+    if ($total % 10 == 0) {
+        $total = floor($total / 10);
+    } else {
+        $total = floor($total/10)+1;
+    }
+    echo $total;
 }
