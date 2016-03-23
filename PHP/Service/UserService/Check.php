46ce3b9709db19a5d2ed16d4f7b7009ceebd0185
yourname <liujiannan@imudges.com>
2016-03-23 12:48:02 +0800

    wanshan

--- PHP/Service/UserService/Check.php
+++ PHP/Service/UserService/Check.php
@@ -12,8 +12,32 @@ function check($table)
 {
     if (isset($_SESSION['userid'])) {
         $getuser = $_SESSION['userid'];
+        $array1 = null;
+        $array2 = null;
+        $conn = connect();
         $str = "select * from {$table} where GetUser = {$getuser}";
-        $row = sel($str);
-        echo ch_json_encode($row);
+        $result = mysqli_query($conn,$str);
+        $row = mysqli_fetch_assoc($result);
+        $i = 0;
+        while($row)
+        {
+            $array1[$i] = $row;
+            $i++;
+            $row = mysqli_fetch_assoc($result);
+        }
+        $conn = connect();
+        $str = "select * from {$table} where UserId = {$getuser}";
+        $result = mysqli_query($conn,$str);
+        $row = mysqli_fetch_assoc($result);
+        $i = 0;
+        while($row)
+        {
+            $array2[$i] = $row;
+            $i++;
+            $row = mysqli_fetch_assoc($result);
+        }
+        $array['get'] = $array1;
+        $array['post'] = $array2;
+        echo ch_json_encode($array);
     }
 }
