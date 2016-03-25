beb421e8a3b3f50962513d9a3f1110c509459290
yourname <liujiannan@imudges.com>
2016-03-25 15:00:43 +0800

    wanshan

--- PHP/Service/GiveService/UP.php
+++ PHP/Service/GiveService/UP.php
@@ -9,22 +9,17 @@ session_start();
 require_once '../../DAO/DAO.php';
 require_once '../UserService/Buffer.php';
 require_once '../UserService/Upload.php';
+if (isset($_SESSION['userid'])) {
+    $src = $_FILES['file']['tmp_name'];
+    $mm = get_md5_string();
+    $N = $mm . '.png';
+    $path = 'http://7xrqhs.com1.z0.glb.clouddn.com/' . $mm . '.png';
+    upload($src, $N, 'give');
+    unlink($src);
+    $_SESSION['bjbj'] = $path;
+    echo "1";
+}
 function get_md5_string()
 {
     return md5(uniqid(microtime(true), true));
-}
-function MP($str)
-{
-//修改头像
-    if (isset($_SESSION['userid'])) {
-        $userid = $_SESSION['userid'];
-        $src = $str;
-        $mm = get_md5_string();
-        $N=$mm.'.png';
-        $path = 'http://7xrqhs.com1.z0.glb.clouddn.com/' . $mm .'.png';
-        upload($src,$N, 'give');
-        unlink($src);
-        $_SESSION['bjbj'] = $path;
-        echo "1";
-    }
 }
