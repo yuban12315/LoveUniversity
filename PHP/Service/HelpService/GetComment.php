abdb17997df595d8dc68a8e5c05bba31369b0787
yourname <liujiannan@imudges.com>
2016-03-25 14:00:20 +0800

    wanshan

---
+++ PHP/Service/HelpService/GetComment.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LC
+ * Date: 2016/3/25
+ * Time: 13:51
+ */
+require_once '../UserService/Comment.php';
+@$page = (int)$_GET['page'];
+comment('helpcomment',$page,'HelpId',1,'CommentId');
